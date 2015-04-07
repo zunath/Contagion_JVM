2a1bfcedbeb2f667d7fd2a991e0bcfbd06252df1
zunath <coolty3001@yahoo.com>
2015-04-07 18:51:35 -0500

    Segfault fix on rest event. Fix permission check when no flag is in range for structure system

--- src/contagionJVM/Event/Module_OnPlayerRest.java
+++ src/contagionJVM/Event/Module_OnPlayerRest.java
@@ -2,20 +2,24 @@ package contagionJVM.Event;
 import contagionJVM.Dialog.DialogManager;
 import contagionJVM.IScriptEventHandler;
 import org.nwnx.nwnx2.jvm.*;
+import org.nwnx.nwnx2.jvm.constants.RestEventtype;
 
 @SuppressWarnings("unused")
 public class Module_OnPlayerRest implements IScriptEventHandler {
 	@Override
 	public void runScript(final NWObject objSelf) {
-        NWObject oPC = NWScript.getLastPCRested();
+        final NWObject oPC = NWScript.getLastPCRested();
+        int restType = NWScript.getLastRestEventType();
 
-        Scheduler.assign(oPC, new Runnable() {
+        if(restType != RestEventtype.REST_STARTED ||
+                oPC.equals(NWObject.INVALID)) return;
+
+        Scheduler.assignNow(oPC, new Runnable() {
             @Override
             public void run() {
                 NWScript.clearAllActions(false);
             }
         });
-
         DialogManager.startConversation(oPC, oPC, "RestMenu");
 	}
 }
--- src/contagionJVM/System/StructureSystem.java
+++ src/contagionJVM/System/StructureSystem.java
@@ -595,12 +595,16 @@ public class StructureSystem {
 
     public static boolean PlayerHasPermission(NWObject oPC, int permissionID, int flagID)
     {
+        if(flagID <= 0) return true;
+
         PlayerGO pcGO = new PlayerGO(oPC);
         StructureRepository repo = new StructureRepository();
         PCTerritoryFlagPermissionEntity permission = repo.GetPermissionByID(pcGO.getUUID(), permissionID, flagID);
         PCTerritoryFlagEntity entity = repo.GetPCTerritoryFlagByID(flagID);
 
-        return permission != null || entity.getPlayerID().equals(pcGO.getUUID());
+        if(permission == null || entity == null) return false;
+        else if (entity.getPlayerID().equals(pcGO.getUUID())) return true;
+        else return true;
     }
 
 }
